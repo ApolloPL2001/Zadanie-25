d3aca8d4115185abbefe93dacb9f99004ad2a293
AdamIwinski <ama5432112345w@wp.pl>
2024-01-19 12:59:57 +0100

    feature-medu_waleciprosto

--- prostopadloscianiwalec.c
+++ prostopadloscianiwalec.c
@@ -37,9 +37,31 @@ void cuboid_field_service () {
     printf("Objentosc prostokonta: %f\n", cuboid_field(a, b, c));
     }
 
-int main () {
-    cuboid_field_service();
-    cyliner_voliume_service();
-    return(0);
+void menu() {
+    int choice;
+    printf("Wybierz opcje:\n");
+    printf("1. Policz objentosc prostopaldoscianu\n");
+    printf("2. Policz objentosc walca\n");
+
+    printf("Wybor:");
+    scanf("%d", &choice);
+
+    if (choice == 1){
+        cuboid_field_service();
+
+        }
+    else if (choice == 2){
+        cyliner_voliume_service();
+
     }
+    else {
+        printf("Nieprawidlowy wybor\n");
+    }
+}
+
+int main () {
+    menu();
+
+    return(0);
+}
 
